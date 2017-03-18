2b9a2222e6abfe1c1a04ffef9a980032c019b6d8
luckytoilet <bai.li.2005@gmail.com>
2017-03-18 14:23:55 -0400

    JJ Lin Melody

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from music21 import *
+from music21 import converter
+from music21.tempo import MetronomeMark
 
-littleMelody = converter.parse("tinynotation: 3/4 c4 d8 f g16 a g f#")
-littleMelody.show('midi')
+# If Only by JJ Lin
+JJ_LIN_MELODY = """
+tinynotation: 4/4
+r2 r8 G8 e8 d8     e2 e8 d8 e8 c8     B8 g8 g4 e8 d8 e8 c8
+A4 a4 g8 f8 e8 f8     e2 e8 d8 e8 c8    A4 a4 g8 f8 e8 f8
+g2~ g8 c16 d16 e8 d8    e8 c16 d16 e8 d8 e8 c16 d16 e8 d8
+f1
+"""
 
-#littleMelody.show()
+melody = converter.parse(JJ_LIN_MELODY)
+melody.insert(0, MetronomeMark(number=95))
+
+melody.show('midi')
+#melody.show()
--- readme.txt
+++ readme.txt
@@ -1,4 +1,6 @@
 Setup
 1. sudo pip install -r requirements.txt
 2. Create ~/.music21rc using instructions here: http://web.mit.edu/music21/doc/tutorials/environment.html
-3. Add midipath to music21rc, eg: <preference name="midiPath" value="/usr/bin/timidity" />
+3. Add midipath to music21rc, eg: <preference name="midiPath" value="/usr/bin/vlc" />
+4. install musescore (sudo apt-get install musescore)
+5. Add musicxmlPath to music21rc, value = "/usr/bin/musescore"
