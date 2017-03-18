d1b1de3f5e3126ad2752d062ae274a9b1258a470
luckytoilet <bai.li.2005@gmail.com>
2017-03-18 15:00:30 -0400

    Manually specify chords and play at same time

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from music21 import converter
 from music21.tempo import MetronomeMark
+from music21.chord import Chord
+from music21.note import Rest
+from music21.duration import Duration
+from music21.stream import Stream
+
 
 # If Only by JJ Lin
 JJ_LIN_MELODY = """
@@ -10,8 +15,31 @@ g2~ g8 c16 d16 e8 d8    e8 c16 d16 e8 d8 e8 c16 d16 e8 d8
 f1
 """
 
+WHOLE_NOTE = Duration(4.0)
+
+C_MAJOR = Chord(["c3", "e3", "g3", "c4"], duration=WHOLE_NOTE)
+G_MAJOR = Chord(["g2", "b2", "d3", "g3"], duration=WHOLE_NOTE)
+A_MINOR = Chord(["a2", "c3", "e3", "a3"], duration=WHOLE_NOTE)
+E_MINOR = Chord(["e3", "g3", "b3", "e4"], duration=WHOLE_NOTE)
+F_MAJOR = Chord(["f2", "a2", "c3", "f3"], duration=WHOLE_NOTE)
+
+chords = converter.parse("""tinynotation: 4/4""")
+chords.append(Rest(duration=WHOLE_NOTE))
+chords.append(Chord(C_MAJOR))
+chords.append(Chord(G_MAJOR))
+chords.append(Chord(A_MINOR))
+chords.append(Chord(G_MAJOR))
+chords.append(Chord(F_MAJOR))
+chords.append(Chord(E_MINOR))
+chords.append(Chord(F_MAJOR))
+chords.append(Chord(G_MAJOR))
+
 melody = converter.parse(JJ_LIN_MELODY)
 melody.insert(0, MetronomeMark(number=95))
 
-melody.show('midi')
-#melody.show()
+song = Stream()
+song.insert(0, melody)
+song.insert(0, chords)
+
+song.show('midi')
+#song.show()
