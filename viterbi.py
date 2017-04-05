e644ce763aff07752a210d67c5bd502b617bb77e
Michael Tu <tuzhucheng@outlook.com>
2017-04-04 20:24:28 -0400

    change to emission function

--- viterbi.py
+++ viterbi.py
@@ -1,13 +1,13 @@
 import operator
 
 
-def viterbi(states, obs, init_probs, trans_probs, emit_probs):
+def viterbi(states, obs, init_probs, trans_probs, emit_fn):
     """
     :param states: the state space
     :param obs: a list of observations
     :param init_probs: a list of initial probabilities
     :param trans_probs: transition probability matrix
-    :param emit_probs: emission probability matrix
+    :param emit_fn: emission probability function
     """
     # V[i][j] stores probability of most likely path for the first j observations that have i as the final state
     V = [[0 for _ in range(len(obs))] for _ in range(len(states))]
@@ -15,12 +15,12 @@ def viterbi(states, obs, init_probs, trans_probs, emit_probs):
 
     # Initialization
     for i in range(len(states)):
-        V[i][0] = init_probs[i] * emit_probs[i][obs[0]]
+        V[i][0] = init_probs[i] * emit_fn(states[i], obs[0])
         back_ptrs[i][0] = 0
 
     for i in range(1, len(obs)):
         for j in range(len(states)):
-            V[j][i] = emit_probs[j][obs[i]] * max(V[k][i-1] * trans_probs[k][j] for k in range(len(states)))
+            V[j][i] = emit_fn(states[j], obs[i]) * max(V[k][i-1] * trans_probs[k][j] for k in range(len(states)))
             # argmax
             back_ptrs[j][i], _ = max(enumerate([V[k][i-1] * trans_probs[k][j] for k in range(len(states))]), key=operator.itemgetter(1))
 
@@ -42,8 +42,11 @@ if __name__ == '__main__':
     states = ['Healthy', 'Fever']
     init_probs = [0.6, 0.4]
     trans_probs = [[0.7, 0.3], [0.4, 0.6]]
-    emit_probs = [{'normal': 0.5, 'cold': 0.4, 'dizzy': 0.1}, {'normal': 0.1, 'cold': 0.3, 'dizzy': 0.6}]
-    seq_prob, seq = viterbi(states, observations, init_probs, trans_probs, emit_probs)
+    def emit_fn(state, obs):
+        vals = {'Healthy': {'normal': 0.5, 'cold': 0.4, 'dizzy': 0.1},
+                'Fever': {'normal': 0.1, 'cold': 0.3, 'dizzy': 0.6}}
+        return vals[state][obs]
+
+    seq_prob, seq = viterbi(states, observations, init_probs, trans_probs, emit_fn)
     print('Most like sequence is', seq)
     print('This sequence has probability', seq_prob)
-
